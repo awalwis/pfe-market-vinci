f06522049858709ba06004914da791ea0f532b04
AntoineGitHub7 <77532418+AntoineGitHub7@users.noreply.github.com>
2021-12-09 11:20:10 +0100

    add env.development

--- src/services/users.js
+++ src/services/users.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const apiurl = 'https://pfe-market-vinci.herokuapp.com//api/utilisateurs'
+const apiurl = 'https://pfe-market-vinci.herokuapp.com/api/utilisateurs'
 
 const getAll = () => {
     return axios.get(apiurl)
