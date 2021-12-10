ed9d045efb7ca096134e99e843b2ffe4914d9f98
alexnightborn <alexandre.walwis@student.vinci.be>
2021-12-10 11:49:05 +0100

    fix config ci

--- App.test.js
+++ App.test.js
@@ -1,7 +1,4 @@
-import { render } from '@testing-library/react';
-import Enzyme, { mount } from 'enzyme';
-import App from './App';
-import User from 'components/User/User';
+import Enzyme from 'enzyme';
 
 // Add your adapter version below
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
