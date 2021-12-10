586beb5e92529988ad5494b6f31fcbff99a70fcc
alexnightborn <alexandre.walwis@student.vinci.be>
2021-12-10 12:16:43 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Register from "components/Register/Register";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "components/Login/Login";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./src/components/Home/Home";
+import Home from "./components/Home/Home";
 
 const App = () => {
     
