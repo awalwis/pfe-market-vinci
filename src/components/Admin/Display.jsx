fc590aee5ca5e8c9f7d9420f8041c4c661f89d44
Nicolas MATTHIJSSEN <nicolas.matthijssen@student.vinci.be>
2021-12-11 19:30:19 +0100

    add - redirection

--- src/components/Admin/Display.jsx
+++ src/components/Admin/Display.jsx
@@ -1,34 +1,39 @@
 import React from 'react'
 import { Table } from 'react-bootstrap';
+import {useHistory} from "react-router-dom";
 import "styles/style.css"
-
 export default function Display(props) {
 
-const displayUsers = (props) => {
+const DisplayUsers = (props) => {
 
     const {users} = props; 
+    const history = useHistory(); 
+
+    const navigateToUserProfile = (email) =>{
+        history.push("/profile/"+email);
+    }
 
     if(users.length > 0) {
         return (
             users.map((user) => {
                 return(
-                    <tr className="tuple" key={user.id_user} >
-                        <td>
-                            {user.last_name}
-                        </td>
-                        <td>
-                            {user.first_name}
-                        </td>
-                        <td>
-                            {user.email}
-                        </td>
-                        <td>
-                            {user.campus}
-                        </td>
-                        <td>
-                            {user.role}
-                        </td>
-                    </tr>    
+                    <tr className="tuple" key={user.id_user} onClick={e => navigateToUserProfile(user.email)} >
+                            <td>
+                                {user.last_name}
+                            </td>
+                            <td>
+                                {user.first_name}
+                            </td>
+                            <td>
+                                {user.email}
+                            </td>
+                            <td>
+                                {user.campus}
+                            </td>
+                            <td>
+                                {user.role}
+                            </td>
+                    </tr>
                 )
             })
         )
@@ -57,7 +62,7 @@ return (
                 </tr>
             </thead>
             <tbody>
-                    {displayUsers(props)}
+                    {DisplayUsers(props)}
             </tbody>
         </Table>
     </>
