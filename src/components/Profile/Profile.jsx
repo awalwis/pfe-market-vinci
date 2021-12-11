500eabc436bdc30273660048d0f2ceb11aab1591
Sbao17 <simon.bao@student.vinci.be>
2021-12-11 16:48:07 +0100

    merge

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,54 @@
+import { useState } from "react";
 import {Button} from "react-bootstrap";
 import {useParams} from "react-router-dom";
 import {userService} from "../../services/users.service";
 
-const Profile = async () => {
+/* const GetCurrentUser= ()=>{
 
     const email = useParams().email
-    let currentUser;
-    await userService.getByEmail(email).then(response => {
-        currentUser = response.data.user;
-        console.log(currentUser)
-    })
-    console.log("current: ", currentUser)
-    if (currentUser)
-        return (
-            <div>
-                <p>Nom: {currentUser.last_name}</p>
-                <p>Prenom: {currentUser.first_name}</p>
-                <p>Mail: {currentUser.email}</p>
-                <p>Campus: {currentUser.campus}</p>
-                <p>Role: {currentUser.role}</p>
-                <p>Mot de passe: {currentUser.password}</p>
-            </div>
-        )
-    else return <>
-        <p>Veuillez vous connecter</p>
-        <Button href="/login" variant="outline-primary" type="submit">
-            Se connecter
-        </Button>
-    </>
+    console.log("email ",email)
+
+     userService.getByEmail(email).then(response => {
+     let currentUser
+     currentUser = response.data.user;
+     console.log(currentUser)
+     if (currentUser)
+         return (
+             <div>
+                 <p>Nom: {currentUser.last_name}</p>
+                 <p>Prenom: {currentUser.first_name}</p>
+                 <p>Mail: {currentUser.email}</p>
+                 <p>Campus: {currentUser.campus}</p>
+                 <p>Role: {currentUser.role}</p>
+                 <p>Mot de passe: {currentUser.password}</p>
+             </div>
+         )
+     else return <>
+         <p>Veuillez vous connecter</p>
+         <Button href="/login" variant="outline-primary" type="submit">
+             Se connecter
+         </Button>
+          </>
+      })
+      return(
+          <></>
+      )
+    } */
+const Profile =  () => {
+
+    
+   
+
+    return(
+        <div>
+            à implémenter
+        </div>
+        
+    )
 }
+
+   
+   
+
 
 export default Profile;
