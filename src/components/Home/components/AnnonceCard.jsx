3f94ee91216d1acb9d814a88739ffe2d3c76ec0e
alex <alexandre.walwis@student.vinci.be>
2021-12-15 07:27:20 +0100

    first attempt

--- src/components/Home/components/AnnonceCard.jsx
+++ src/components/Home/components/AnnonceCard.jsx
@@ -1,4 +1,7 @@
+import { Box, Card } from "@mui/material";
+imp
 import { Container } from "react-bootstrap";
+import Label from "./Label";
 
 const AnnonceCard = ({annonce}) => {
 
@@ -8,16 +11,37 @@ const AnnonceCard = ({annonce}) => {
     }
     
     return(
-        <Container className="border" style={{"height":"200px", "width":"150px", "cursor":"pointer"}}
+        <Card>
+            <Box sx={{ pt: '100%', position: 'relative' }}>
+                {status && (
+                <Label
+                    variant="filled"
+                    color={(status === 'sale' && 'error') || 'info'}
+                    sx={{
+                    zIndex: 9,
+                    top: 16,
+                    right: 16,
+                    position: 'absolute',
+                    textTransform: 'uppercase'
+                    }}
+                >
+                    {status}
+                </Label>
+                )}
+                <ProductImgStyle alt={name} src={cover} />
+            </Box>
+            <Container className="border" style={{"height":"200px", "width":"150px", "cursor":"pointer"}}
             onClick={(e) =>
                 {e.preventDefault()
                 showDetails(annonce.id_ad)}}>
-            <div>{annonce.title}</div>
-            <div>{annonce.description}</div>
-            <div>{annonce.price}</div>
-            <div>{annonce.date}</div>
-            <div>{annonce.type==="give"?"À donner":"À vendre"}</div>
-        </Container>
+                <div>{annonce.title}</div>
+                <div>{annonce.description}</div>
+                <div>{annonce.price}</div>
+                <div>{annonce.date}</div>
+                <div>{annonce.type==="give"?"À donner":"À vendre"}</div>
+            </Container>
     )
+        </Card>
+        
 }
 export default AnnonceCard;
